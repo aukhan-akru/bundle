2b1444088ef331f38b269f83f59452b01846f74f
Asadullah khan <aukhan@akru.co>
2022-03-30 10:51:32 +0500

    commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import {camelCase} from "lodash"
+
+
+function doSomeThing(){
+console.log(camelCase("Hello world"))
+
+var tag = document.createElement("p");
+var text = document.createTextNode("Tutorix is the best e-learning platform");
+tag.appendChild(text);
+var element = document.getElementById("new");
+element.appendChild(tag);
+
+}
+
+
